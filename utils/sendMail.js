25c5dc369c96d3bfef15f3003216eda339157e01
César Tonatiuh Tamayo Ávila <cesar.tamayo@alumnos.udg.mx>
2021-01-08 20:43:08 -0600

    Correccion correo 3

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -5,12 +5,30 @@ const USER = process.env.GMAIL_EMAIL;
 const PASS = process.env.GMAIL_PASS;
 
 async function sendMail(email, url) {
+  // const transporter = nodemailer.createTransport({
+  //   // service: "gmail",
+  //   service: "hotmail",
+  //   auth: {
+  //     user: USER,
+  //     pass: PASS,
+  //   },
+  // });
+
   const transporter = nodemailer.createTransport({
-    // service: "gmail",
-    service: "hotmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
-      user: USER,
-      pass: PASS,
+      type: "OAuth2",
+      user: "lomeetoinfo@gmail.com",
+      clientId:
+        "298460132135-mlb1tjr3uncleans8u5e85qihgh6jq55.apps.googleusercontent.com",
+      clientSecret: "GL9J0nXldWzOX8qjLxvI1OA1",
+      refreshToken:
+        "1//04kGLj2b-t8f9CgYIARAAGAQSNwF-L9Ir5iDDDjS8HeC-YO3rM6z8AJ1sWNsx-QgU3AhAX2qtn_U1fKcx2YOuywl6d_M8Fk2yxnY",
+      accessToken:
+        "ya29.a0AfH6SMAOJyvQEViRTAZNUf6eV_29S6l1tlx14_KGeBROZ6YFnVTnlqTM4glqfM53TVAEaRyK94gCd42f67-tAkeDILtiYQ_W8vE9HEwzKsXgohEQqvtc-SeGdtqWWdbO0BlrXB_yhEijXnuuIeMZ3kJk1DAIBVzlyx9mf_FPUCQ",
+      expires: 3599,
     },
   });
 
