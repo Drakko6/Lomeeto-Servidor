bfc12aacdfb32c44134c194087f13f6b2e603fea
César Tonatiuh Tamayo Ávila <cesar.tamayo@alumnos.udg.mx>
2021-01-08 19:12:58 -0600

    Inicial

---
+++ utils/sendMail.js
@@ -0,0 +1,32 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config({ path: ".env" });
+
+const USER = process.env.GMAIL_EMAIL;
+const PASS = process.env.GMAIL_PASS;
+
+async function sendMail(email, url) {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: USER,
+      pass: PASS,
+    },
+  });
+
+  const info = await transporter.sendMail({
+    from: " <lomeetoinfo@gmail.com>", // sender address
+    to: email, // list of receivers
+    subject: "Confirmacion de Usuario ✔", // Subject line
+    text: "Confirma tu correo haciendo clic en el link", // plain text body
+    html: `Confirma tu correo haciendo clic en el link
+          <a href="${url}"> ${url} </a>`, // html body
+  });
+
+  console.log(info);
+
+  console.log("Message sent: %s", info.messageId);
+
+  // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+}
+
+module.exports = sendMail;
